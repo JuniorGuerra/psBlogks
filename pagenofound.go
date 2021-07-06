748e70fa98c86c512b612d1ac3b84e0b1c6fb450
JuniorGuerra <juniorguerrac17@gmail.com>
2021-07-05 20:38:44 -0500

    nose

--- pagenofound.go
+++ pagenofound.go
@@ -7,6 +7,6 @@ import (
 // Handle404 ...
 func Handle404() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hola esta pagina esta caida"))
+		w.Write([]byte("Error 404 esta pagina no tiene existencia"))
 	})
 }
