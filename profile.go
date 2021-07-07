4035f26b019a1af2a3332c42872033b1e8ce8d6c
JuniorGuerra <juniorguerrac17@gmail.com>
2021-07-06 21:41:24 -0500

    añadir el diseño de pestaña profile

--- profile.go
+++ profile.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func handle_perfil(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("user")
+	tmp, _ := template.ParseFiles("public/profile/index.html")
+	tmp.Execute(w, nil)
+
+	/*cookie, err := r.Cookie("user")
 	if err != nil {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
 	}
+
+
 	fmt.Println("Interfaz de perfil de usuario 1.01")
 	fmt.Fprintf(w, "<h1>%s</h1>", cookie.Value)
+	*/
 
 }
