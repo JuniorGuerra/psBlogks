813ce06208a3ba7c419f0b3f7d98c1b5df36d051
JuniorGuerra <juniorguerrac17@gmail.com>
2021-07-08 20:20:43 -0500

    añadir editar el perfil y modificar el edit.go para mostrar los datos

--- edit.go
+++ edit.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func handle_edit_profile(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>In this page is for edit profile</h1>"))
+	tmpl, _ := template.ParseFiles("public/edit_profile/index.html")
+	tmpl.Execute(w, nil)
 }
 
 func handle_edit_book(w http.ResponseWriter, r *http.Request) {
